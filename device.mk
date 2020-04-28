3ea9c5d71fa89d684411a42b0bdaea14dab71339
xcore995 <arman.p95@gmail.com>
2020-04-28 08:51:28 -0400

    krillin: Add danielhk's camera.client wrapper

--- device.mk
+++ device.mk
@@ -189,10 +189,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    libcam.client \
     Snap
 
 # Default Propierties
