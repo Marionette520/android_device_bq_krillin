1e1c29591ad4ff93bd40c203c72cc5f736b5023f
xcore995 <arman.p95@gmail.com>
2020-04-28 08:57:53 -0400

    krillin: Port sensorlistener for camera.  * Till oreo sensor stuff was in libgui, now moved to new libsensor.  * Making shim causes other sensors stop work.

--- device.mk
+++ device.mk
@@ -191,6 +191,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
     libcam.client \
+    libcam.utils.sensorlistener \
     Snap
 
 # Default Propierties
