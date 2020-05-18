366ff8b6afba161aa74d20984fab2065af69be1b
jmpfbmx <jmpf.bmx@gmail.com>
2020-05-19 01:01:12 +0200

    krillin: Clean up and restructure lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,28 +1,28 @@
-# Full base
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit framework first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Needed stuff
+# Languages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Call device specific makefile
+# Inherit from krillin device
 $(call inherit-product, device/bq/krillin/device.mk)
-
-LOCAL_PATH := device/bq/krillin
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=7.1.2/MRA58M/2419427:user/release-keys PRIVATE_BUILD_DESC="krillin-user 7.1.2 MRA58M 2419427 release-keys"
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="krillin"
-
-# IO Scheduler
-PRODUCT_PROPERTY_OVERRIDES += \
-    sys.io.scheduler=bfq    
-       
-PRODUCT_NAME := lineage_krillin
-PRODUCT_DEVICE :=krillin
-PRODUCT_BRAND := bq
-PRODUCT_MANUFACTURER := bq
-PRODUCT_MODEL := Aquaris E4.5
 
 # Correct bootanimation size for the screen
 TARGET_SCREEN_HEIGHT := 960
@@ -32,9 +32,30 @@ TARGET_SCREEN_WIDTH := 540
 PRODUCT_AAPT_CONFIG := normal mdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 CM_BUILD :=krillin
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := lineage_krillin
+PRODUCT_DEVICE :=krillin
+PRODUCT_BRAND := bq
+PRODUCT_MANUFACTURER := bq
+PRODUCT_MODEL := Aquaris E4.5
+
+# IO Scheduler
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.io.scheduler=bfq
+
+# Set product device & name
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_DEVICE="krillin"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=7.1.2/MRA58M/2419427:user/release-keys \
+    PRIVATE_BUILD_DESC="krillin-user 7.1.2 MRA58M 2419427 release-keys"
+
+# Additional Features
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
 ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
 
@@ -45,5 +66,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
   persist.service.adb.enable=1 \
   persist.service.debuggable=1 \
   persist.sys.usb.config=mtp,adb
-
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
